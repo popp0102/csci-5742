b4247c1525881800b9177e7b7da8b9920f11b2ba
jpopp <jason@getnotion.com>
2022-01-30 11:58:25 -0700

    stub methods

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,1 +1,21 @@
-print("Hello world")
+def parse_args():
+    print("parse argv")
+
+def sniff_traffic():
+    print("sniff trafic")
+
+def calculate_rates():
+    print("calculate rates")
+
+def print_summary(results):
+    print("print results")
+
+def main():
+    args       = parse_args()
+    hash_table = sniff_traffic()
+    results    = calculate_rates()
+    print_summary(results)
+
+if __name__ == "__main__":
+    main()
+
