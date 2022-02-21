6e83d3f10f27d569383b097dab287de27b9b0a65
jpoppler <jason@getnotion.com>
2022-02-21 14:42:45 -0700

    don't use 3.9 method
    Just in case the TA uses a lower version

--- hw2/lib/command_line_parser.py
+++ hw2/lib/command_line_parser.py
@@ -10,7 +10,7 @@ import argparse
 ############################################################
 def cmd_parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", action=argparse.BooleanOptionalAction)
+    parser.add_argument("-v", "--verbose", action='store_true')
     parser.add_argument("-s", "--fps",  help="the fanout rate threshold (per second) that indicates the source is a port scanner (default 5)", type=int, default=5)
     parser.add_argument("-m", "--fpm",  help="the fanout rate threshold (per minute) that indicates the source is a port scanner (default 100)", type=int, default=100)
     parser.add_argument("-l", "--fp5m", help="the fanout rate threshold (per 5 minutes) that indicates the source is a port scanner (default 300)", type=int, default=300)
