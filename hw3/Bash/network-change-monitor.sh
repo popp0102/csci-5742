d8fcf827afaf8590ded47f5ac264c8eeea0baae7
jpoppler <popp0102@gmail.com>
2022-03-01 19:48:11 -0700

    add comments to code

--- hw3/Bash/network-change-monitor.sh
+++ hw3/Bash/network-change-monitor.sh
@@ -3,6 +3,14 @@
 ##############################################################################
 # Function: scan_network()
 #
+# This method scans the network using nmap on the passed in subnet. It will
+# check all ports from 1-65535 on each host in the subnet. After running nmap
+# the output is parsed and processed line by line. It first captures the host
+# it finds in the list. Then the open ports will follow. This method assumes
+# nmap output will always be of the same form (host then ports displayed).
+# It uses the regexes provided below to parse and capture the relevant host
+# and port numbers. It then places them in an associative array to be later
+# processed.
 ##############################################################################
 scan_network() {
   subnet=$1
@@ -18,7 +26,6 @@ scan_network() {
   do
     if [[ $line =~ $ip_regex ]]; then
       current_host=${BASH_REMATCH[1]}
-      HOSTS+=$current_host
     fi
 
     if [[ $line =~ $port_regex ]]; then
@@ -31,12 +38,18 @@ scan_network() {
 ##############################################################################
 # Function: display_network_differences()
 #
+# This method checks the differences from the last scan and the scan from the
+# current run. It does this by cycling through the last scanned hosts and checks
+# if it's in the current scanned hosts. If it's not in the scanned hosts, that
+# means it was recently opened. In either case we can remove the host_port 
+# combo from the SCANNED_HOSTS list because of this check and whatever is left
+# over in this scanne hosts port list must be new host/port pairs that were opened.
 ##############################################################################
 display_network_differences() {
   for host_port in "${!LAST_SCAN_HOSTS[@]}"
   do
     if ! [[ ${SCANNED_HOSTS["$host_port"]} == open ]]; then
-      log_message "OPEN on $host_port"
+      log_message "CLOSE on $host_port"
     fi
 
     unset SCANNED_HOSTS["$host_port"]
@@ -44,13 +57,15 @@ display_network_differences() {
 
   for host_port in "${!SCANNED_HOSTS[@]}"
   do
-    log_message "CLOSE on $host_port"
+    log_message "OPEN on $host_port"
   done
 }
 
 ##############################################################################
 # Function: save_scanned_network()
 #
+# This  method simply saves the scanned hosts to a text file located in the
+# HOST_DB_FILE variable.
 ##############################################################################
 save_scanned_network() {
   log_message "Saving scanned hosts to $HOST_DB_FILE"
@@ -63,6 +78,9 @@ save_scanned_network() {
 ##############################################################################
 # Function: load_last_scan()
 #
+# This method will read in the last scanned hosts. This will be later used to
+# check differences in the current scan. Note this method will rm the file as
+# it will be created later when it gets saved.
 ##############################################################################
 load_last_scan() {
   if ! [ -f $HOST_DB_FILE ]; then
@@ -92,6 +110,10 @@ log_message() {
 ##############################################################################
 # Function: main()
 #
+# Entrypoint method to setup variables and run the high level methods. First
+# we load the last scan, scan the current network, save that scan, then detect
+# and display any differences. The method takes one argument which is a CIDR
+# subnet string (eg 192.168.86.0/24).
 ##############################################################################
 main() {
   LOGFILE='network-log.txt'
