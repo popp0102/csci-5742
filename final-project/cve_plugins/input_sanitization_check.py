d58953999b2a82fdba570e71831c095fea15ad94
jpoppler <jason@getnotion.com>
2022-04-24 13:28:26 -0600

    implement a rudimentary input sanitzation plugin
    Detect if a node is an input() statement. If so then further check if
    the variable it's set to is ever used on the LHS of any other statement.
    If it isn't then notify the user. In addition, f an Expr or Return statment
    contain an input() statement in them also notify.

--- final-project/cve_plugins/input_sanitization_check.py
+++ final-project/cve_plugins/input_sanitization_check.py
@@ -1,3 +1,5 @@
+import pdb
+import re
 import astroid
 from typing            import TYPE_CHECKING
 from astroid           import nodes
@@ -24,10 +26,31 @@ class InputSanitizationChecker(BaseChecker):
         ),
     }
     options = ()
+    INPUT_REGEX = "^input(\w*)"
 
     def __init__(self, linter: PyLinter =None) -> None:
         super(InputSanitizationChecker, self).__init__(linter)
 
+    def is_input_statement(self, value):
+        if re.search(self.INPUT_REGEX, value.as_string()):
+            return True
+        return False
+
     def visit_functiondef(self, node) -> None:
-        print("111111111111111")
+        lhs_of_input_dict = {}
+        for statement in node.body:
+            node_type = statement.__class__.__name__
+            if node_type in ['Expr', 'Return']:
+                if self.is_input_statement(statement.value):
+                    self.add_message('input-sanitize-check', node=statement)
+                for lhs_name, original_statement in lhs_of_input_dict.items():
+                    if node_type == 'Return' and statement.value.as_string() ==  lhs_name:
+                        self.add_message('input-sanitize-check', node=statement)
+            elif node_type in ['Assign']:
+                for target in statement.targets:
+                    target_name = target.as_string()
+                    if self.is_input_statement(statement.value):
+                        lhs_of_input_dict[target_name] = statement
+                    elif target_name in lhs_of_input_dict:
+                        del lhs_of_input_dict[target_name]
 
--- final-project/tests/input_sanitization_test.py
+++ final-project/tests/input_sanitization_test.py
@@ -1,7 +1,30 @@
-def get_user_input():
-    print("Please enter something")
-    return input()
+import html
 
-user_input = get_user_input()
-print("The user entered: " + user_input)
+def get_user_input_without_sanitization3():
+    user_input = input("Please enter something")
+    return user_input
+
+def get_user_input_without_sanitization():
+    input("Please enter something")
+
+def get_user_input_without_sanitization2():
+    return input("Please enter something")
+
+def get_user_input_with_sanitization():
+    print("Please enter something")
+    user_input = input()
+    user_input = html.escape(user_input)
+    return user_input
+
+user_input = get_user_input_without_sanitization()
+print("You entered: " + user_input)
+
+user_input = get_user_input_without_sanitization2()
+print("You entered: " + user_input)
+
+user_input = get_user_input_without_sanitization3()
+print("You entered: " + user_input)
+
+user_input = get_user_input_with_sanitization()
+print("You entered: " + user_input)
 
