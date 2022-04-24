b640aa4729ae83dc3514702f90ffa29957108f7d
kamaroyl <kamaroyl@gmail.com>
2022-04-24 12:12:19 -0600

    Install requires and requirements cleanup

--- final-project/setup.py
+++ final-project/setup.py
@@ -6,10 +6,16 @@ setup(name='CSCI_5742_Final_Project',
       version='1.0',
       description='Python Static Analysis for Common Weaknesses',
       author='Jason Poppler, Benjamin Straub',
-      author_email='jason.poppler@ucdenver.edu, benjamin.straub@ucdenver.edu',
+      author_email='jason.poppler@ucdenver.edu; benjamin.straub@ucdenver.edu',
       url='https://github.com/popp0102/csci-5742',
       packages=['cve_plugins'],
       package_dir={'cve_plugins': 'cve_plugins'},
-      py_modules=['ban_arbitrary_execution_subprocess', 'ban_create_os_subprocess', 'cwe1'],
-      requires=[]
+      py_modules=[
+            'ban_arbitrary_execution_subprocess',
+            'ban_create_os_subprocess',
+            'cwe1'],
+      install_requires=[
+            'astroid==2.9.3',
+            'pylint==2.12.2'
+      ]
      )
